51728b7bd09f130fb4d70009161a4a998da5a9c5
Danny Danhammer <49801209+DannyDanhammer@users.noreply.github.com>
2024-02-04 22:27:08 -0600

    Update htbstart.py
    Adams additions

--- htbstart.py
+++ htbstart.py
@@ -1,26 +1,42 @@
-
 #!/bin/bash
 
 # Prompt user for input
 read -p "Enter the IP address: " ip_address
 read -p "Enter the hostname: " hostname
 
-# Set environmental variable
-export TARGET="$ip_address"
 
-# Construct the command to update /etc/hosts
-update_hosts_command="echo \"$ip_address $hostname\" | sudo tee -a /etc/hosts"
+# https://www.linuxjournal.com/content/validating-ip-address-bash-script
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+    	
 
-# Execute the command to update /etc/hosts
-eval "$update_hosts_command"
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+        
+        # verify that we received only one argument:
+        if [ $# -gt 1 ]
+  	then
+    	    echo "Failed validation, additional data supplied."
+    	    stat=1
+	fi
+    fi
+    return $stat
+}
 
-# Construct the nmap command
-nmap_command="sudo nmap -p\$(nmap -p- --min-rate=1000 -T4 $TARGET -Pn | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//) -sC -sV -Pn -vvv $TARGET -oN nmap_tcp_all.nmap"
+# validate IP Address
+if ! valid_ip $ip_address 
+then 
+    echo "Invalid IP Address defined" 
+    exit 0 
+fi
 
-# Execute the nmap command
-eval "$nmap_command"
 
-
-##chmod +x htbstart
-# note: maybe try adding openvpn start to the script as wellbut must include time delay of 40 seconds. Ideas Adam? I know you better be reading this super vulnerable code....I mean look at me running unvalidated eval !
-
+echo "valid ip: $ip_address"
